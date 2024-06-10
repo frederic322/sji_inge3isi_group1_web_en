28687497ed2e8e0064657f8151483ee2435a8cb2
Frederic322 <onanafrederic32@gmail.com>
2024-06-10 20:42:36 +0100

    crud done arrangement of the folders soon

--- Employee/js/script.js
+++ Employee/js/script.js
@@ -49,38 +49,42 @@ file.onchange = function(){
 }
 
 
-function showInfo(){
-    document.querySelectorAll('.employeeDetails').forEach(info => info.remove())
-    getData.forEach((element, index) => {
-        let createElement = `<tr class="employeeDetails">
-            <td>${index+1}</td>
-            <td><img src="${element.picture}" alt="" width="50" height="50"></td>
-            <td>${element.employeeName}</td>
-            <td>${element.employeeSector}</td>
-            <td>${element.employeeActivity}</td>
-            <td>${element.employeeEmail}</td>
-            <td>${element.employeePhone}</td>
-            <td>${element.employeeRegion}</td>
-            <td>${element.employeeDepartment}</td>
-            <td>${element.employeeDistrict}</td>
-            <td>${element.employeeNeighborhood}</td>
-            <td>${element.employeepLocation}</td>
+function showInfo() {
+  // Clear existing employee details
+  document.querySelectorAll('.employeeDetails').forEach(info => info.remove());
 
+  // Use getData directly for all pills
+  getData.forEach((element, index) => {
+    let createElement = `
+      <tr class="employeeDetails">
+        <td>${index + 1}</td>
+        <td><img src="${element.picture}" alt="" width="50" height="50"></td>
+        <td>${element.employeeName}</td>
+        <td>${element.employeeSector}</td>
+        <td>${element.employeeActivity}</td>
+        <td>${element.employeeEmail}</td>
+        <td>${element.employeePhone}</td>
+        <td>${element.employeeRegion}</td>
+        <td>${element.employeeDepartment}</td>
+        <td>${element.employeeDistrict}</td>
+        <td>${element.employeeNeighborhood}</td>
+        <td>${element.employeepLocation}</td>
 
-            <td>
-                <button class="btn btn-success" onclick="readInfo('${element.picture}', '${element.employeeName}', '${element.employeeEmail}', '${element.employeePhone}', '${element.employeeSector}', '${element.employeeActivity}', '${element.employeeRegion}', '${element.employeeDepartment}', '${element.employeeDistrict}','${element.employeeNeighborhood}','${element.employeepLocation}')" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
+        <td>
+          <button class="btn btn-success" onclick="readInfo('${element.picture}', '${element.employeeName}', '${element.employeeEmail}', '${element.employeePhone}', '${element.employeeSector}', '${element.employeeActivity}', '${element.employeeRegion}', '${element.employeeDepartment}', '${element.employeeDistrict}','${element.employeeNeighborhood}','${element.employeepLocation}')" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
 
-                <button class="btn btn-primary" onclick="editInfo('${index}', '${element.picture}', '${element.employeeName}', '${element.employeeEmail}', '${element.employeePhone}', '${element.employeeSector}', '${element.employeeActivity}', '${element.employeeRegion}', '${element.employeeDepartment}', '${element.employeeDistrict}','${element.employeeNeighborhood}','${element.employeepLocation}')" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
+          <button class="btn btn-primary" onclick="editInfo('${index}', '${element.picture}', '${element.employeeName}', '${element.employeeEmail}', '${element.employeePhone}', '${element.employeeSector}', '${element.employeeActivity}', '${element.employeeRegion}', '${element.employeeDepartment}', '${element.employeeDistrict}','${element.employeeNeighborhood}','${element.employeepLocation}')" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
 
-                <button class="btn btn-danger" onclick="deleteInfo(${index})"><i class="bi bi-trash"></i></button>
+          <button class="btn btn-danger" onclick="deleteInfo(${index})"><i class="bi bi-trash"></i></button>
 
-            </td>
-        </tr>`
+        </td>
+      </tr>`;
 
-        userInfo.innerHTML += createElement
-    })
+    userInfo.innerHTML += createElement;
+  });
 }
 showInfo()
+
 
 
 function readInfo(pic, name, email, phone, sector, activity, region, department,district,neighborhood,plocation){
@@ -91,25 +95,6 @@ function readInfo(pic, name, email, phone, sector, activity, region, department,
     document.querySelector("#ShowSector").value = sector;
     document.querySelector("#Showactivity").value = activity;
     document.querySelector("#ShowRegion").value = region;
-
-    /* Update Region selection
-    const regionSelect = document.getElementById("ShowRegion");
-    for (let i = 0; i < regionSelect.options.length; i++) {
-      if (regionSelect.options[i].value === region) {
-        regionSelect.selectedIndex = i;
-        break;
-      }
-    }
-
-    const departmentSelect = document.getElementById("ShowDepartment");
-    for (let i = 0; i < departmentSelect.options.length; i++) {
-      if (departmentSelect.options[i].value === department) {
-        departmentSelect.selectedIndex = i;
-        break;
-      }
-    }*/
-
-
     document.querySelector("#ShowDepartment").value = department;
     document.querySelector("#ShowDistrict").value = district;
     document.querySelector("#ShowNeighborhood").value = neighborhood;
@@ -123,15 +108,19 @@ function editInfo(index, pic, name, email, phone, sector, activity, region, depa
     editId = index,
     imgInput.src = pic,
     userName.value = name,
-    email.value = email,
-    phone.value = phone,
-    sector.value = sector,
-    activity.value = activity,
+    document.getElementById("email").value = email,
+    document.getElementById("phone").value = phone,
+    document.getElementById("Sector").value = sector,
+    document.getElementById("activity").value = activity,
     Region.value = region,
+    Region.length = 1;
     department.value = department,
     district.value = district,
     neighborhood.value = neighborhood,
     pLocation.value = plocation
+    updateLocationSelection()
+
+
 
     submitBtn.innerText = "Update"
     modalTitle.innerText = "Update The Form"
@@ -139,12 +128,14 @@ function editInfo(index, pic, name, email, phone, sector, activity, region, depa
 
 
 function deleteInfo(index){
-    if(confirm("Are you sure want to delete?")){
-        getData.splice(index, 1)
-        localStorage.setItem("userProfile", JSON.stringify(getData))
-        showInfo()
-    }
+  if(confirm("Are you sure want to delete?")){
+      getData.splice(index, 1)
+      localStorage.setItem("userProfile", JSON.stringify(getData))
+      showInfo()
+  }
 }
+
+
 
 
 form.addEventListener('submit', (e)=> {
@@ -166,6 +157,8 @@ form.addEventListener('submit', (e)=> {
 
     if(!isEdit){
         getData.push(information);
+        //first show before alert
+        alert('Employee created successfully'); // success
     }
     else{
         getData[editId] = information;
@@ -207,120 +200,143 @@ var RegionSatinfo={
 
 },
 
-// Littoral:{
-//     Moungo:{
-//         Douala:{
-//             Akwa:{
-//                 Bonajo:["lycee Akwa"],
-//                 Deido:["Rond point Deido","Maison deido"]
-//         }
+Littoral:{
+    Moungo:{
+        Douala:{
+            Akwa:{
+                Bonajo:["lycee Akwa"],
+                Deido:["Rond point Deido","Maison deido"]
+        }
 
-//     },
-//     Nkam:{
-//         Bonaberi:{
-//            Mkepe:{
-//                 Tignere:["lycee Tignere"],
-//                 Kadey:["Rond point Kadey","bomba"]
-//         }
-
-//     }
-// }
-// }
-// }
-  }
-
-window.onload= function()
-{
-  const selectRegion = document.getElementById('Region'),
-  selectDepartment = document.getElementById('Department'),
-  selectDistrict = document.getElementById('District'),
-  selectNeighborhood = document.getElementById('Neighborhood'),
-  selectLocation = document.getElementById('pLocation'),
-  selects =  document.querySelectorAll('select')
-
-
-  selectDepartment.disabled = true
-  selectDistrict.disabled = true
-  selectNeighborhood.disabled = true
-  selectLocation.disabled = true
-
-  selects.forEach(select => {
-      if(select.disabled== true){
-          select.style.cursor= "auto"
-      }
-  })
-  for(let Region in RegionSatinfo){
-      // console.log(Region);
-      selectRegion.options[selectRegion.options.length] = new Option(Region,Region)
-
-  }
-  //region change
-  selectRegion.onchange= (e) =>{
-      selectDepartment.disabled = false
-      selectDistrict.disabled = true
-      selectNeighborhood.disabled = true
-      selectLocation.disabled = true
-
-      selectDepartment.length=1
-      selectDistrict.length=1
-      selectNeighborhood.length=1
-      selectLocation.length=1
-
-      for(let Department in RegionSatinfo[e.target.value]){
-          console.log(Department);
-          selectDepartment.options[selectDepartment.options.length]=new Option(Department,Department)
-      }
-
-  }
-
-
-
-
-  //Depart change
-  selectDepartment.onchange= (e) =>{
-      selectDistrict.disabled = false
-      selectNeighborhood.disabled = true
-      selectLocation.disabled = true
-
-      selectDistrict.length=1
-      selectNeighborhood.length=1
-      selectLocation.length=1
-
-      for(let District in RegionSatinfo[selectRegion.value][e.target.value]){
-          console.log(District);
-          selectDistrict.options[selectDistrict.options.length]=new Option(District,District)
-      }
-  }
-   //District change
-   selectDistrict.onchange=(e) =>{
-
-      selectNeighborhood.disabled = false
-      selectLocation.disabled = true
-
-
-      selectNeighborhood.length=1
-      selectLocation.length=1
-
-      for(let Neighborhood in RegionSatinfo[selectRegion.value][selectDepartment.value][e.target.value]){
-          console.log(Neighborhood);
-          selectNeighborhood.options[selectNeighborhood.options.length]=new Option(Neighborhood,Neighborhood)
-      }
-  }
-
-//Neighborhood change
-selectNeighborhood.onchange=(e) =>{
-
-  selectLocation.disabled = false
-
-
-  selectLocation.length=1
-
-  let Locations = RegionSatinfo[selectRegion.value][selectDepartment.value][selectDistrict.value][selectNeighborhood.value]
-
-  for(let i=0;i<Locations.length; i++){
-      selectLocation.options[selectLocation.options.length]= new Option(Locations[i],Locations[i])
-  }
+    },
+    Nkam:{
+        Bonaberi:["Rond point Kadey","bomba"]
 }
-
 }
+}
+  }
+
+
+
+    function updateLocationSelection() {
+        const selectRegion = document.getElementById('Region'),
+          selectDepartment = document.getElementById('Department'),
+          selectDistrict = document.getElementById('District'),
+          selectNeighborhood = document.getElementById('Neighborhood'),
+          selectLocation = document.getElementById('pLocation'),
+          selects = document.querySelectorAll('select');
+
+        // Disable location select elements initially
+        selectDepartment.disabled = true;
+        selectDistrict.disabled = true;
+        selectNeighborhood.disabled = true;
+        selectLocation.disabled = true;
+
+        // Set cursor to default for disabled elements
+        selects.forEach(select => {
+          if (select.disabled === true) {
+            select.style.cursor = "auto";
+          }
+        });
+
+        // Populate Region select element
+        for (let region in RegionSatinfo) {
+          selectRegion.options[selectRegion.options.length] = new Option(region, region);
+        }
+
+        // Region Change Event Listener
+        selectRegion.onchange = (e) => {
+          selectDepartment.disabled = false;
+          selectDistrict.disabled = true;
+          selectNeighborhood.disabled = true;
+          selectLocation.disabled = true;
+
+          selectDepartment.length = 1; // Clear previous department options
+          selectDistrict.length = 1; // Clear previous district options
+          selectNeighborhood.length = 1; // Clear previous neighborhood options
+          selectLocation.length = 1; // Clear previous location options
+
+          // Populate Department select element based on selected Region
+          for (let department in RegionSatinfo[e.target.value]) {
+            selectDepartment.options[selectDepartment.options.length] = new Option(department, department);
+          }
+        };
+
+        // Department Change Event Listener
+        selectDepartment.onchange = (e) => {
+          selectDistrict.disabled = false;
+          selectNeighborhood.disabled = true;
+          selectLocation.disabled = true;
+
+          selectDistrict.length = 1; // Clear previous district options
+          selectNeighborhood.length = 1; // Clear previous neighborhood options
+          selectLocation.length = 1; // Clear previous location options
+
+          // Populate District select element based on selected Region and Department
+          for (let district in RegionSatinfo[selectRegion.value][e.target.value]) {
+            selectDistrict.options[selectDistrict.options.length] = new Option(district, district);
+          }
+        };
+
+        // District Change Event Listener
+        selectDistrict.onchange = (e) => {
+          selectNeighborhood.disabled = false;
+          selectLocation.disabled = true;
+
+          selectNeighborhood.length = 1; // Clear previous neighborhood options
+          selectLocation.length = 1; // Clear previous location options
+
+          // Populate Neighborhood select element based on selected Region, Department, and District
+          for (let neighborhood in RegionSatinfo[selectRegion.value][selectDepartment.value][e.target.value]) {
+            selectNeighborhood.options[selectNeighborhood.options.length] = new Option(neighborhood, neighborhood);
+          }
+        };
+
+        // Neighborhood Change Event Listener
+        selectNeighborhood.onchange = (e) => {
+          selectLocation.disabled = false;
+
+          selectLocation.length = 1; // Clear previous location options
+
+          // Populate Location select element based on selected Region, Department, District, and Neighborhood
+          const locations = RegionSatinfo[selectRegion.value][selectDepartment.value][selectDistrict.value][e.target.value];
+          for (let i = 0; i < locations.length; i++) {
+            selectLocation.options[selectLocation.options.length] = new Option(locations[i], locations[i]);
+          }
+        };
+      }
+      updateLocationSelection();
+
+// Get the input field
+let phoneInput = document.getElementById('phone');
+
+// Get the error message element
+let errorMessage = document.getElementById('error-message');
+
+// Listen for keypress event
+phoneInput.addEventListener('keypress', function(e) {
+    // Get the key pressed
+    let key = e.key;
+
+    // Check if the key is not a digit
+    if (!/\d/.test(key)) {
+        // Prevent the key from being entered
+        e.preventDefault();
+
+        // Display an error message in red
+        errorMessage.style.display = 'block';
+        errorMessage.style.color = 'red'; // Set the color to red
+        errorMessage.innerText = 'Please enter only numbers for the phone number';
+    } else {
+        // Hide the error message if the key is a digit
+        errorMessage.style.display = 'none';
+    }
+});
+
+
+
+
+
+
+
 
