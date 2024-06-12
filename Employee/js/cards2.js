c0279e9efb3e084c453d2b6eb0e3a4477c8dcbd9
Leslinte <leslinefoyet793@gmail.com>
2024-06-12 14:37:50 +0100

    popup

--- Employee/js/cards2.js
+++ Employee/js/cards2.js
@@ -75,3 +75,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
         overlay.style.display = 'none';
     });
 });
+// Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the card that opens the modal
+// var card = document.getElementById("triggerCard");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementById("closeModalBtn");
+
+// // When the user clicks on the card, open the modal
+// card.onclick = function() {
+//   modal.style.display = "flex";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
